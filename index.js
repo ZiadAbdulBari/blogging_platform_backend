53c107f793e2647311734e81373086eec7ed449e
ZiadAbdulBari <ziadabdulbari01@gmail.com>
2024-02-29 21:12:50 +0600

    ratelimiter added

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import limiter from "./middleware/rateLimiter.js";
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 import 'dotenv/config'
 app.use(cors());
-
+app.use(limiter)
 import auth from './routes/auth.route.js';
 
 app.use('/api/v1/auth/',auth);
